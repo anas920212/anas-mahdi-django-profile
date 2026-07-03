460f54552a29f4f5679077dfd191fc1e66c3e62c
anas920212 <70367463+anas920212@users.noreply.github.com>
2026-07-03 20:01:04 +0200

    Add Procfile and update production settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn anasmahdi_portfolio.wsgi
--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*$+j+d-)_t&3h%mqh-+=z+vbccmheoya59-gw6&ogb06#kfif*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
